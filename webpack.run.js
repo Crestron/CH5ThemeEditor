78c79a64671da1304220b89e38095672fb86873e
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2021-11-23 13:17:53 +0530

    Code Refactoring
    Code Refactoring

--- webpack.run.js
+++ webpack.run.js
@@ -8,7 +8,7 @@ const common = require('./webpack.build.js');
 const glob = require('glob');
 let dotenv = require('dotenv').config().parsed;
 
-const isDevMode = ["--watch","--watchmode"];
+const isDevMode = ["--watch", "--watchmode"];
 //Ignore env inputs if app is running.
 dotenv = !!process.argv[4] && isDevMode.includes(process.argv[4]) ? undefined : dotenv;
 const fontsSourceFilepath = !!dotenv && !!dotenv.SOURCE_FONTS_FILE_PATH ? dotenv.SOURCE_FONTS_FILE_PATH : 'output/fonts/*';
@@ -17,6 +17,8 @@ const webFontsSourceFilepath = !!dotenv && !!dotenv.SOURCE_WEBFONTS_FILE_PATH ? 
 const webFontsDestinationFilepath = !!dotenv && !!dotenv.DESTINATION_WEBFONTS_FILE_PATH ? dotenv.DESTINATION_WEBFONTS_FILE_PATH : './app/dist/webfonts/';
 const basePath = path.resolve(__dirname);
 const sourceFilePath = !!dotenv && !!dotenv.SOURCE_THEMES_FILE_PATH ? dotenv.SOURCE_THEMES_FILE_PATH : 'output/themes/*.css';
+const fontAwesomeFilePath = "./node_modules/@fortawesome/fontawesome-free";
+const materialIconsFilePath = "./node_modules/@material-icons/font";
 
 // get the all theme list
 const manifestSourceFilePath = "./app.manifest.json";
@@ -24,12 +26,11 @@ const manifestDestFilePath = "./app/dist/manifest/";
 const themeList = {};
 themeList.themeName = glob.sync('output/themes/*-theme.css');
 const jsonData = JSON.stringify(themeList, null, 4);
-fs.writeFileSync(manifestSourceFilePath,jsonData);
+fs.writeFileSync(manifestSourceFilePath, jsonData);
 
-
-let destinationFilePath = !!dotenv && !!dotenv.DESTINATION_THEMES_FILE_PATH
-  ? dotenv.DESTINATION_THEMES_FILE_PATH
-  : './app/dist/css/';
+let destinationFilePath = !!dotenv && !!dotenv.DESTINATION_THEMES_FILE_PATH ? dotenv.DESTINATION_THEMES_FILE_PATH : './app/dist/css/';
+let fontAwesomeDestinationFilePath = !!dotenv && !!dotenv.DESTINATION_THEMES_FILE_PATH ? dotenv.DESTINATION_THEMES_FILE_PATH : './app/dist/css/font-awesome/';
+let materialIconsDestinationFilePath = !!dotenv && !!dotenv.DESTINATION_THEMES_FILE_PATH ? dotenv.DESTINATION_THEMES_FILE_PATH : './app/dist/css/material-icons/';
 
 const searches = ['--outputpath'];
 let transformURL = function (cliUrl) {
@@ -43,11 +44,13 @@ let transformURL = function (cliUrl) {
   return cliDestPath;
 };
 
-let pathMatches = _.filter(process.argv, (item) => { return item.indexOf(searches[0]) !== -1});
+let pathMatches = _.filter(process.argv, (item) => { return item.indexOf(searches[0]) !== -1 });
 
 if (pathMatches.length) {
   let processArgv = transformURL(pathMatches);
   destinationFilePath = !processArgv['outputpath'] ? destinationFilePath : processArgv.outputpath;
+  fontAwesomeDestinationFilePath = !processArgv['outputpath'] ? fontAwesomeDestinationFilePath : processArgv.outputpath + '/font-awesome/';
+  materialIconsDestinationFilePath = !processArgv['outputpath'] ? materialIconsDestinationFilePath : processArgv.outputpath + '/material-icons/';
 }
 module.exports = merge(common, {
   mode: !!process.argv[4] && isDevMode.includes(process.argv[4]) ? 'development' : 'production',
@@ -55,28 +58,48 @@ module.exports = merge(common, {
     new CopyPlugin([
       {
         from: path.resolve(basePath, sourceFilePath),
-        //Relative path for destination folder
         to: path.resolve(basePath, destinationFilePath),
         flatten: true,
         force: true
       },
       {
         from: path.resolve(basePath, fontsSourceFilepath),
-        //Relative path for destination folder
         to: path.resolve(basePath, fontsDestinationFilepath),
         flatten: true,
         force: true
       },
       {
+        from: path.resolve(basePath, fontAwesomeFilePath + "/css/all.css"),
+        to: path.resolve(basePath, fontAwesomeDestinationFilePath + "/css/all.css"),
+        flatten: true,
+        force: true
+      },
+      {
+        from: path.resolve(basePath, fontAwesomeFilePath + "/webfonts/*"),
+        to: path.resolve(basePath, fontAwesomeDestinationFilePath + "/webfonts"),
+        flatten: true,
+        force: true
+      },
+      {
+        from: path.resolve(basePath, materialIconsFilePath + "/css/all.css"),
+        to: path.resolve(basePath, materialIconsDestinationFilePath + "/css/all.css"),
+        flatten: true,
+        force: true
+      },
+      {
+        from: path.resolve(basePath, materialIconsFilePath + "/font/*"),
+        to: path.resolve(basePath, materialIconsDestinationFilePath + "/font"),
+        flatten: true,
+        force: true
+      },
+      {
         from: path.resolve(basePath, webFontsSourceFilepath),
-        //Relative path for destination folder
         to: path.resolve(basePath, webFontsDestinationFilepath),
         flatten: true,
         force: true
       },
       {
         from: path.resolve(basePath, manifestSourceFilePath),
-        //Relative path for destination folder
         to: path.resolve(basePath, manifestDestFilePath),
         flatten: true,
         force: true
