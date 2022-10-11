ac0891ed9ed02273c026373e674655038fca5984
Raghavendra Kumar Dabbir <rdabbir@crestron.com>
2022-10-11 15:12:12 +0530

    fix: Changes to Case sensitive CH5TextInput
    Changes to Case sensitive CH5TextInput

--- sass-schema/index.ts
+++ sass-schema/index.ts
@@ -150,7 +150,7 @@ async function initialize() {
     'ch5-select': 'Ch5Select',
     'ch5-slider': 'Ch5Slider',
     'ch5-spinner': 'Ch5Spinner',
-    'ch5-textinput': 'Ch5Textinput',
+    'ch5-textinput': 'Ch5TextInput',
     'ch5-toggle': 'Ch5Toggle',
     'ch5-video': 'Ch5Video',
     'ch5-dpad': 'Ch5Dpad',
